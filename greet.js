9ea262c0f7ec701ca20f4aa3fb4bbc0c5d54fd81
Tenzin Choeyang <tenchoespajr@gmail.com>
2023-03-25 23:44:10 +0530

    added js file

---
+++ greet.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('.btn')
+const content = document.querySelector('.content')
+
+btn.addEventListener('click',()=>{
+    content.innerHTML="Hello everyone It's me your boy evilmonster"
+})
